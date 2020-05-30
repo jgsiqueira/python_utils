36a6e580554108e49374010d0e49dda40cd749fe
Jorge Gabriel Siqueira <jorge.gab.siqueira@gmail.com>
2020-05-30 14:00:45 -0700

    Updating facebook.py script with new methods

--- facebook.py
+++ facebook.py
@@ -2,18 +2,40 @@ import requests
 import json
 import sys
 
-GRAPH_API_URL = "https://graph.facebook.com/v2.10/"
+GRAPH_API_URL = "https://graph.facebook.com/"
 
-def page_scrapper(page, access_token):
+#
+# You need an app published with the permissions listed on each method to access pages you don't own
+# If you want to just check your own page, you can get a page token in https://developers.facebook.com/tools/explorer/
+#
+APP_ID = "YOUR_APP_ID"
+APP_SECRET = "YOUR_APP_SECRET"
+
+def get_fb_token(app_id, app_secret):
+    url = 'https://graph.facebook.com/oauth/access_token'       
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': app_id,
+        'client_secret': app_secret
+    }
+    response = requests.post(url, params=payload)
+    return response.json()['access_token']
+
+#
+# Necessary permissions: pages_read_engagement
+#
+def get_page_top_posts(page, access_token):
     page_info_url = GRAPH_API_URL + page + "?fields=fan_count&access_token=" + access_token
     feed_url = GRAPH_API_URL + page + "/feed?fields=shares,comments.limit(0).summary(true),reactions.limit(0).summary(true),created_time&access_token=" + access_token
 
     r = requests.get(page_info_url)
     data = json.loads(r.text)
+
     numberOfLikes = data["fan_count"]
 
     r = requests.get(feed_url)
     data = json.loads(r.text)
+
     posts = data["data"]
 
     likes = []
@@ -32,7 +54,11 @@ def page_scrapper(page, access_token):
 
             r_insights = requests.get(post_reach_url)
             data_insights = json.loads(r_insights.text)
-            reachs.append(data_insights["data"][0]["values"][0]["value"])
+
+            if len(data_insights["data"]) > 0:
+                reachs.append(data_insights["data"][0]["values"][0]["value"])
+            else:
+                reachs.append(0)
 
             if ("reactions" in post) and ("summary" in post["reactions"]):
                 likes.append(post["reactions"]["summary"]["total_count"])
@@ -56,5 +82,39 @@ def page_scrapper(page, access_token):
         else:
             posts = []
 
+    # Wrap the result in a single dict
+    result = {}
+
     for i in range(0,len(ids)):
-        print str(time[i]) + "," + "https://www.facebook.com/"+str(ids[i]) + "," + str(likes[i]) + "," + str(comments[i]) + "," + str(shares[i]) + "," + str(reachs[i])
+        result[ids[i]] = {}
+        result[ids[i]]["likes"] = likes[i]
+        result[ids[i]]["comments"] = comments[i]
+        result[ids[i]]["shares"] = shares[i]
+        result[ids[i]]["reachs"] = reachs[i]
+
+    return result
+
+#
+# Necessary permissions: pages_read_engagement
+#
+def get_post_details(post_id):
+    post_url = GRAPH_API_URL + post_id + "?fields=message,attachments&access_token=" + access_token
+
+    r = requests.get(post_url)
+    data = json.loads(r.text)
+
+    print(data)
+
+#
+# Necessary permissions: pages_read_engagement, pages_manage_posts
+#
+def publish_post(page, message):
+    page_url = GRAPH_API_URL + page + "/feed?access_token=" + access_token
+    
+    post_data = {}
+    post_data["message"] = message
+
+    r = requests.post(page_url, data=post_data)
+    data = json.loads(r.text)
+
+    print(data)
